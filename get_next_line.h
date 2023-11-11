58e40484bc5d37b00073a82c40970a1dd290f743
jfikents <jfikents@student.42Heilbron.de>
2023-11-11 21:37:46 +0100

    working get_next_line

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 11:34:54 by JFikents          #+#    #+#             */
-/*   Updated: 2023/11/08 15:06:46 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/11/11 16:41:01 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@
 # include <stdio.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 10
 # endif
 
 char			*get_next_line(int fd);
 void			*ft_calloc(unsigned long count, unsigned long size);
 char			*ft_strchr(char *src, int c);
 unsigned long	ft_strlen(const char *str);
-char			*ft_handle_nl(char *line);
+char			*ft_handle_nl(char *line, char *last_line, int check_read);
+long			ft_back2zero(char **pre_line, char **tmp,
+					char **pos, char *last_read);
 
 #endif
